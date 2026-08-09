55b3706407d629d842424316f1b64fc271631e7f
Rafal <rafal.z.malucha@gmail.com>
2026-08-09 15:41:45 +0200

    implement basic message bus

--- Assets/_Scripts/MessageBus.cs
+++ Assets/_Scripts/MessageBus.cs
@@ -1,16 +1,54 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class MessageBus
 {
+    private Dictionary<Type, List<Delegate>> _handlers = new();
+
+    public MessageBus()
+    {
+        Debug.Log("Message Bus init");
+    }
+
     public void Subscribe<T>(Action<T> handler)
     {
+        Type messageType = typeof(T);
+
+        if (!_handlers.ContainsKey(messageType))
+        {
+            _handlers.Add(messageType, new List<Delegate>());
+        }
+
+        _handlers[messageType].Add(handler);
+
+        Debug.Log(handler + " sub");
     }
 
     public void Unsubscribe<T>(Action<T> handler)
     {
+        Type messageType = typeof(T);
+
+        if (!_handlers.ContainsKey(messageType))
+            return;
+
+        _handlers[messageType].Remove(handler);
+
+        Debug.Log(handler + " unsub");
     }
 
     public void Publish<T>(T message)
     {
+        Debug.Log(message);
+
+        Type messageType = typeof(T);
+
+        if (!_handlers.ContainsKey(messageType))
+            return;
+
+        foreach (Delegate handler in _handlers[messageType])
+        {
+            ((Action<T>)handler)(message);
+        }
     }
 }
