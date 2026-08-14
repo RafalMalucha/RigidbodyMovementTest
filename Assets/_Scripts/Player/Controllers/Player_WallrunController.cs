de4782fea5e7a098c11889e9c51da71316030dca
Rafal <rafal.z.malucha@gmail.com>
2026-08-14 23:24:56 +0200

    Some improvements to wallrun cancelling

--- Assets/_Scripts/Player/Controllers/Player_WallrunController.cs
+++ Assets/_Scripts/Player/Controllers/Player_WallrunController.cs
@@ -1,13 +1,15 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Player_WallrunController : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private BoxCollider _rWallrunCheck;
-    [SerializeField] private BoxCollider _lWallrunCheck;
+    [SerializeField] private float _maxWallrunDuration;
 
     private Player_State _currentState;
+    private Vector3 _wallNormal;
+    private float _wallrunStartTime;
 
     void OnEnable()
     {
@@ -34,15 +36,55 @@ public class Player_WallrunController : MonoBehaviour
     void OnPlayerJumpMessageReceived(Player_JumpMessage message)
     {
         Debug.Log("wallrun controller jump");
+        if (_currentState == Player_State.WallRunning)
+        {
+            StartCoroutine(WallrunCancel());
+        }
     }
 
     void OnPlayerWallrunEnterMessageReceived(Player_WallrunEnterMessage message)
     {
-
+        _wallNormal = message.WallNormal;
+        Wallrun();
     }
 
     void OnPlayerWallrunExitMessageReceived(Player_WallrunExitMessage message)
     {
+        Debug.Log("wallrun end");
+    }
 
+    private void Wallrun()
+    {
+        Debug.Log(_wallNormal);
+        _wallrunStartTime = Time.time;
+
+        // while (Time.time < _wallrunStartTime + _maxWallrunDuration)
+        // {
+        //     //Debug.Log("wallrun time");
+        // }
+        //WallrunCancel();
+    }
+
+    // private void WallrunCancel()
+    // {
+    //     Debug.Log("wallrun cancel jump");
+    //     _rigidbody.AddForce(_wallNormal * 300f, ForceMode.Impulse);
+    //     _rigidbody.AddForce(transform.rotation * Vector3.forward * 200f, ForceMode.Impulse);
+
+    //     //_rigidbody.AddForce(Vector3.up * 500f, ForceMode.Impulse);
+    // }
+
+    // IEnumerator Wallrun()
+    // {
+
+    // }
+
+    IEnumerator WallrunCancel()
+    {
+        _rigidbody.AddForce(_wallNormal * 300f, ForceMode.Impulse);
+        _rigidbody.AddForce(transform.rotation * Vector3.forward * 200f, ForceMode.Impulse);
+        yield return new WaitForFixedUpdate();
+        _rigidbody.AddForce(Vector3.up * 100f, ForceMode.Impulse);
+        yield return new WaitForFixedUpdate();
     }
 }
