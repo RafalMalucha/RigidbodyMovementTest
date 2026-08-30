6f70f283a27ad753b35e12a853f6e61dec431fbc
Rafal <rafal.z.malucha@gmail.com>
2026-08-30 21:00:07 +0200

    add melee attack, change enemy on melee hit behavior

--- Assets/_Scripts/Enemy/Enemy_HealthController.cs
+++ Assets/_Scripts/Enemy/Enemy_HealthController.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Enemy_HealthController : MonoBehaviour
 {
     [Header("Entity_HealthController Setup")]
     [SerializeField] private int _maxBaseHealth;
     [SerializeField] private UnityEvent<int> _updateHealthUI;
 
+    private Rigidbody _rigidbody;
     private int _currentHealth;
 
     private void OnEnable()
     {
+        _rigidbody = GetComponent<Rigidbody>();
         _currentHealth = _maxBaseHealth;
         _updateHealthUI?.Invoke(_currentHealth);
     }
@@ -37,11 +40,18 @@ public class Enemy_HealthController : MonoBehaviour
 
     private void EntityDie()
     {
-        Destroy(transform.parent.gameObject);
+        Destroy(transform.gameObject);
     }
 
     public int GetCurrentHealth()
     {
         return _currentHealth;
+    }
+
+    public void EnemyOnMeleeHit(int hitDamage, int hitForce, Vector3 hitPushDirection)
+    {
+        _rigidbody.AddForce(hitPushDirection * hitForce, ForceMode.Impulse);
+
+        ApplyDamage(hitDamage);
     }
 }
--- Assets/_Scripts/Player/Controllers/Player_MeleeController.cs
+++ Assets/_Scripts/Player/Controllers/Player_MeleeController.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player_MeleeController : MonoBehaviour
 {
     [Header("Player_MeleeController Setup")]
-    [SerializeField] private bool _test;
+    [Space]
+    [SerializeField] private Camera _playerCamera;
+    [SerializeField] private LayerMask _hittableLayer;
+
+    [Header("Melee Setup")]
+    [Space]
+    [SerializeField] private Vector3 _meleeBoxcastHalfExtents;
+    [SerializeField] private float _meleeRange;
+    [SerializeField] private int _meleeHitDamage;
+    [SerializeField] private int _meleeHitForce;
+
+    private RaycastHit[] _boxCastHits;
 
     void OnEnable()
     {
@@ -22,6 +34,38 @@ public class Player_MeleeController : MonoBehaviour
 
     void Melee()
     {
-        Debug.Log("player melee behavior");
+        Debug.Log("melee");
+
+        Vector3 origin = transform.position;
+        Vector3 direction = _playerCamera.transform.forward;
+
+        _boxCastHits = Physics.BoxCastAll(
+            _playerCamera.transform.position + _playerCamera.transform.forward * 1.5f,
+            _meleeBoxcastHalfExtents,
+            _playerCamera.transform.forward,
+            transform.rotation,
+            _meleeRange,
+            LayerMask.GetMask("Hittable")
+        );
+
+        foreach (RaycastHit hit in _boxCastHits)
+        {
+            if (hit.collider.GetComponentInParent<Enemy_HealthController>() is Enemy_HealthController enemy)
+            {
+                Debug.Log(hit.collider.name);
+                enemy.EnemyOnMeleeHit(_meleeHitDamage, _meleeHitForce, _playerCamera.transform.forward);
+            }
+        }
+    }
+
+    void Update()
+    {
+        ExtDebug.DrawBoxCastBox(
+            _playerCamera.transform.position + _playerCamera.transform.forward * 1.5f,
+            _meleeBoxcastHalfExtents, transform.rotation,
+            _playerCamera.transform.forward,
+            _meleeRange,
+            Color.coral
+        );
     }
 }
