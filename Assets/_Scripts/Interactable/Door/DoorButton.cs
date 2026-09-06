0c2927fcd5ee646a6f9ed3ae186b8e35c7199306
Rafal <rafal.z.malucha@gmail.com>
2026-09-07 00:06:22 +0200

    door

---
+++ Assets/_Scripts/Interactable/Door/DoorButton.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DoorButton : MonoBehaviour, IInteractable
+{
+    [SerializeField] private UnityEvent _openDoor;
+    [SerializeField] private UnityEvent _closeDoor;
+
+    private bool _doorOpen = false;
+
+    public void Interact()
+    {
+        if (_doorOpen)
+        {
+            Debug.Log("door close button press");
+            _doorOpen = false;
+            _closeDoor?.Invoke();
+        }
+        else
+        {
+            Debug.Log("door open button press");
+            _doorOpen = true;
+            _openDoor?.Invoke();
+        }
+    }
+}
--- Assets/_Scripts/Interactable/Door/DoorScript.cs
+++ Assets/_Scripts/Interactable/Door/DoorScript.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class DoorScript : MonoBehaviour
@@ -17,9 +18,69 @@ public class DoorScript : MonoBehaviour
     [SerializeField] private Vector3 _rightRotationOpen;
     [SerializeField] private Vector3 _rightRotationClosed;
 
-    [Header("is open")]
-    [Space]
-    [SerializeField] private bool _isOpen;
 
+    private bool _isOpen = false;
 
+    private Quaternion _leftOpenRotationQuaternion;
+    private Quaternion _leftClosedRotationQuaternion;
+    private Quaternion _rightOpenRotationQuaternion;
+    private Quaternion _rightClosedRotationQuaternion;
+
+    private void Awake()
+    {
+        _leftOpenRotationQuaternion = Quaternion.Euler(_leftRotationOpen);
+        _leftClosedRotationQuaternion = Quaternion.Euler(_leftRotationClosed);
+        _rightOpenRotationQuaternion = Quaternion.Euler(_rightRotationOpen);
+        _rightClosedRotationQuaternion = Quaternion.Euler(_rightRotationClosed);
+    }
+
+    public void DoorOpen()
+    {
+        _isOpen = true;
+        Debug.Log("Door open script run");
+        StartCoroutine(LerpDoorRotation());
+    }
+
+    public void DoorClose()
+    {
+        _isOpen = false;
+        Debug.Log("Door close script run");
+        StartCoroutine(LerpDoorRotation());
+    }
+
+    IEnumerator LerpDoorRotation()
+    {
+        float duration = 2.0f;
+        float startTime = Time.time;
+
+        while (Time.time < startTime + duration)
+        {
+            float t = (Time.time - startTime) / duration;
+
+            if (_isOpen)
+            {
+                _leftDoor.transform.rotation = Quaternion.Slerp(_leftClosedRotationQuaternion, _leftOpenRotationQuaternion, t);
+                _rightDoor.transform.rotation = Quaternion.Slerp(_rightClosedRotationQuaternion, _rightOpenRotationQuaternion, t);
+            }
+            else
+            {
+                _leftDoor.transform.rotation = Quaternion.Slerp(_leftOpenRotationQuaternion, _leftClosedRotationQuaternion, t);
+                _rightDoor.transform.rotation = Quaternion.Slerp(_rightOpenRotationQuaternion, _rightClosedRotationQuaternion, t);
+            }
+
+            yield return null;
+        }
+
+        if (_isOpen)
+        {
+            _leftDoor.transform.rotation = _leftOpenRotationQuaternion;
+            _rightDoor.transform.rotation = _rightOpenRotationQuaternion;
+        }
+        else
+        {
+            _leftDoor.transform.rotation = _leftClosedRotationQuaternion;
+            _rightDoor.transform.rotation = _rightClosedRotationQuaternion;
+        }
+
+    }
 }
